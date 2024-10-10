23a1b7f86a4ee42178c0082df8120fe39f234a3f
Dinobi <architectstan01@gmail.com>
2024-10-10 16:45:58 +0100

    New updates

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@
 # Update the system package index (for Ubuntu/Debian)
 sudo apt update
 
+
+# Install Python and pip if not available
+sudo apt-get install -y python3 python3-pip
+
+# Verify Python and pip installation
+python3 --version
+pip3 --version
+
 # Install Rust and Cargo (official Rust installer)
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
@@ -15,23 +23,23 @@ rustc --version
 cargo --version
 
 # Install Python dependencies
-pip install fastapi==0.101.0
-pip install uvicorn==0.23.0
-pip install sqlalchemy==1.4.48
-pip install psycopg2-binary==2.9.6
-pip install pydantic==1.10.9
-pip install fastapi-jwt-auth==0.4.0
-pip install python-multipart==0.0.6
-pip install pillow==9.4.0
-pip install web3
-pip install aiofiles==0.8.0
-pip install python-dotenv==1.0.0
-pip install requests==2.28.1
-pip install 'eth-utils>=2.1.0'
-pip install gunicorn==20.1.0
-pip install pysui==0.70.0
-pip install maturin
+pip3 install fastapi==0.101.0
+pip3 install uvicorn==0.23.0
+pip3 install sqlalchemy==1.4.48
+pip3 install psycopg2-binary==2.9.6
+pip3 install pydantic==1.10.9
+pip3 install fastapi-jwt-auth==0.4.0
+pip3 install python-multipart==0.0.6
+pip3 install pillow==9.4.0
+pip3 install web3
+pip3 install aiofiles==0.8.0
+pip3 install python-dotenv==1.0.0
+pip3 install requests==2.28.1
+pip3 install 'eth-utils>=2.1.0'
+pip3 install gunicorn==20.1.0
+pip3 install pysui==0.70.0
+pip3 install maturin
 
 # Confirm the installations
-pip freeze
+pip3 freeze
 
