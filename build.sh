f431eed466ac33898b37196ab16b25d3f9120c10
Dinobi <architectstan01@gmail.com>
2024-10-10 14:42:45 +0100

    New updates

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 
+# Install necessary build tools for compiling native extensions
+apt-get update && apt-get install -y build-essential pkg-config libssl-dev
+
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
